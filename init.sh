94d4b3cd72c5593e611ca558d83f3ca77861b793
Lazzlo2096 <lazzlo2096@yandex.ru>
2018-09-30 18:32:56 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
-sudo apt-get install openbox obconf obmenu
+sudo apt-get update
+sudo apt-get install openbox obconf obmenu | yes #113MB
