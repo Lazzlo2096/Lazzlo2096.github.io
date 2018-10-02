66917ff9e4623b6633cd66d9c0e166a391d6c0f4
Lazzlo2096 <lazzlo2096@yandex.ru>
2018-10-02 21:16:19 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 sudo apt-get update
-sudo apt-get install xorg openbox obconf obmenu #| yes #113MB + xorg=372MB
+yes | sudo apt-get install xorg openbox obconf obmenu #113MB + xorg=372MB
 
 git clone https://github.com/Lazzlo2096/configs2018.git; cd configs2018;
